81b3458002da22a6b2664688dd90064834f3c7b4
Ningyu Liu <ningyuliu@nliu2.physci.fit.edu>
2015-09-14 15:31:18 -0400

    Initial Commit

---
+++ ionizationWave/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ionizationWave
+//
+//  Created by Ningyu Liu on 9/14/15.
+//  Copyright (c) 2015 Ningyu Liu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
